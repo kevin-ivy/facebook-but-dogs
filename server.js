2138f81069aea85b97ebbb83b01ef8d625b1ce92
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-06-25 15:52:40 -0500

    add server

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// will need to update this
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {// automatically sign out user if idle on page more than 10 minutes
+        expires: 10 * 60 * 1000},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// turn on routes
+app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});

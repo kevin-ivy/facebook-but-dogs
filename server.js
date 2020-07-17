7f7083480a8c41697cc88112c2011b680bc40cdd
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-07-16 20:37:13 -0500

    update dogprofile image

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(routes);
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
